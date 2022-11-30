146d6c729de7f8829f5ef35cec97aa67253b4067
Leo mini <a230711@yahoo.com.tw>
2022-11-30 14:07:44 +0800

    修改過往紀錄

--- Index.js
+++ Index.js
@@ -216,7 +216,7 @@ app.put('/finishdeliverorder/:id', async(req, res)=>{
 /* ---------------------------------- */
 /* --------------過往紀錄------------- */
 app.get('/dataslist/:id', async(req, res)=>{
-  const sql = "SELECT deliver_order.order_sid, deliver_order.deliver_order_sid, shop.name AS shopname, shop.address, deliver_fee, member.name, deliver_order.deliver_check_time, deliver_order.deliver_take_time, deliver_order.complete_time FROM( deliver_order INNER JOIN member ON deliver_order.member_sid = member.sid) INNER JOIN shop ON deliver_order.shop_sid = shop.sid WHERE deliver_order.order_finish = 1 AND deliver_order.deliver_sid = ?";
+  const sql = "SELECT deliver_order.order_sid, shop.name AS shopname, shop.address, deliver_fee, member.name, deliver_order.deliver_check_time, deliver_order.deliver_take_time, deliver_order.complete_time FROM( deliver_order INNER JOIN member ON deliver_order.member_sid = member.sid) INNER JOIN shop ON deliver_order.shop_sid = shop.sid WHERE deliver_order.order_finish = 1 AND deliver_order.deliver_sid = ?";
   const [listrow] = await db.query(sql,[req.params.id]);
   res.json(listrow);
 })
