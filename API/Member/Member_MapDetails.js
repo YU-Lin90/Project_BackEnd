998c0b31643ffa4e6a6c96eb55081e13acc14b1c
YuChenLin <andy50152@gmail.com>
2022-12-01 13:16:50 +0800

    地圖、聊天修正

--- API/Member/Member_MapDetails.js
+++ API/Member/Member_MapDetails.js
@@ -33,6 +33,20 @@ router.get("/GetStoreDetail", async (req, res) => {
   // console.log(result);
   res.json(result);
 });
+router.get("/GetOrderAddress", async (req, res) => {
+  const orderSid = req.query.orderSid;
+  if (!orderSid) {
+    return res.json(0);
+  }
+  const memberSid = req.token.sid;
+  // console.log({orderSid,memberSid});
+  const sql =
+    "SELECT o.`receive_address` FROM `orders` o LEFT JOIN `shop` s ON o.`shop_sid` = s.`sid` WHERE o.`member_sid` = ? AND o.`sid` = ?";
+  const [[result]] = await DB.query(sql, [memberSid, orderSid]);
+  const detailSql = "SELECT `sid`, `name`, `price`, `options_type_sid`, `option_order` FROM `options` WHERE 1"
+  // console.log(result);
+  res.json(result);
+});
 //
 router.get("/GetChattingContent", async (req, res) => {
   const orderSid = req.query.orderSid;
