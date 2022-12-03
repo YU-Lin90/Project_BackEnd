72ca84a581baa1968faf83bf28aadd1e57d29319
Leo mini <a230711@yahoo.com.tw>
2022-12-04 03:00:53 +0800

    加入客戶地址

--- routes/deliver.js
+++ routes/deliver.js
@@ -44,7 +44,7 @@ router.post('/sendOrder', async (req, res)=>{
 })
 /* ----------接單後訂單預覽------------- */
 router.get('/deliverorder/:id', async(req, res)=>{
-    const sql1 ="SELECT member.name,  shop.name AS shopname, shop.address, shop.phone, member.name, deliver_order.deliver_memo,  deliver_order.deliver_fee, deliver_order.order_sid FROM (deliver_order INNER JOIN shop ON deliver_order.shop_sid = shop.sid) INNER JOIN member ON deliver_order.member_sid = member.sid WHERE order_sid = ? AND deliver_order.order_finish = 0";
+    const sql1 ="SELECT orders.receive_address, member.name,  shop.name AS shopname, shop.address, shop.phone, member.name, deliver_order.deliver_memo,  deliver_order.deliver_fee, deliver_order.order_sid FROM ((deliver_order INNER JOIN shop ON deliver_order.shop_sid = shop.sid) INNER JOIN member ON deliver_order.member_sid = member.sid) INNER JOIN orders ON deliver_order.store_order_sid = orders.store_order_sid WHERE order_sid = ? AND deliver_order.order_finish = 0";
     const [rows] = await db.query(sql1, [req.params.id]);
     const sql2 ="SELECT products.name, order_detail.product_price, order_detail.amount FROM (order_detail INNER JOIN products ON order_detail.product_sid = products.sid ) WHERE order_detail.order_sid = ?";
     const [food] = await db.query(sql2, [req.params.id]);
