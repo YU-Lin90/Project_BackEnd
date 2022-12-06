c0b92c26ab264f458f86302e13059fc95f0c2023
Yuhung Jian <er6289@gmail.com>
2022-12-06 12:50:26 +0800

    店家銷售分析API加入TODO

--- API/Store/StoreSellAnalyze.js
+++ API/Store/StoreSellAnalyze.js
@@ -12,6 +12,10 @@ router.use("/:sid", async (req, res, next) => {
   // 測試用
   // let storeSid = 89;
 
+  // TODO:折線圖
+  // X:日期 Y:金額
+  // 長條圖(這個月到今天為止 title:XX月商品銷售量)
+  // X:商品名 Y:數量
 
   let sql = `SELECT order_total , order_time FROM orders  WHERE shop_sid = ${sid} ORDER BY sid DESC`;
 
