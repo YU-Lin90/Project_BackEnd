e58e3dd17c699d6e390629d85efff8b17eb95b4a
Aiden4939 <jl4460702@gmail.com>
2022-12-08 04:35:55 +0800

    新增類別拖曳換order的route，修正無法正確傳入type_order給資料庫的問題

--- routes/type.js
+++ routes/type.js
@@ -10,7 +10,8 @@ router.get("/:shop_sid", async (req, res) => {
   };
   const { shop_sid } = req.params;
 
-  const type_sql = "SELECT * FROM products_types WHERE shop_sid=?";
+  const type_sql =
+    "SELECT * FROM products_types WHERE shop_sid=? ORDER BY products_types.type_order";
   const [type_rows] = await db.query(type_sql, [shop_sid]);
 
   const product_sql = "SELECT * FROM `products` WHERE shop_sid=?";
@@ -33,12 +34,18 @@ router.post("/:shop_sid", upload.none(), async (req, res) => {
     error: "資料新增失敗",
   };
 
-  // 計算目前order排到第幾個
-  const sql_order =
-    "SELECT COUNT(1) as num FROM `products_types` WHERE shop_sid=?";
   try {
-    const [[{ num }]] = await db.query(sql_order, [shop_sid]);
-    const order = Number(num + 1);
+    // 找到目前最高的type_order是多少，並+1
+    const sql_order =
+      "SELECT type_order FROM `products_types` WHERE shop_sid=? ORDER BY type_order DESC LIMIT 1";
+    const [[{ type_order: getOrder }]] = await db.query(sql_order, [shop_sid]);
+    const order = Number(getOrder) + 1;
+
+    // 計算目前order排到第幾個
+    // const sql_order =
+    //   "SELECT COUNT(1) as num FROM `products_types` WHERE shop_sid=?";
+    // const [[{ num }]] = await db.query(sql_order, [shop_sid]);
+    // const order = Number(num + 1);
 
     const sql =
       "INSERT INTO `products_types`( `name`, `shop_sid`, `type_order`) VALUES (?,?,?)";
@@ -53,6 +60,17 @@ router.post("/:shop_sid", upload.none(), async (req, res) => {
   }
 
   res.json(output);
+});
+
+router.put("/order", upload.none(), async (req, res) => {
+  console.log(req.body);
+  const sql = "UPDATE `products_types` SET `type_order`=? WHERE sid=?";
+  for (let i = 0; i < req.body.length; i++) {
+    const [result1] = await db.query(sql, [i + 1, req.body[i].sid]);
+  }
+  console.log(123);
+
+  res.send("ok");
 });
 
 // 更新的儲存按鈕按下:
@@ -82,6 +100,7 @@ router.delete("/:sid", async (req, res) => {
   const { sid } = req.params;
   const sql = "DELETE FROM `products_types` WHERE sid=?";
   const result = await db.query(sql, [sid]);
+  res.json(result);
 });
 
 module.exports = router;
