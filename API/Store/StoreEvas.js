ecc68f4edc99193a693aa3164af267c53228d2f0
YuChenLin <andy50152@gmail.com>
2022-12-10 13:05:09 +0800

    店家評價完成

--- API/Store/StoreEvas.js
+++ API/Store/StoreEvas.js
@@ -11,10 +11,13 @@ router.use("/getEvaTotalPages", async (req, res) => {
   const shopSid = req.query.shopSid
   const sql = "SELECT COUNT(1) totalRow FROM `shop_evaluation`  WHERE `shop_sid` = ?"
   const [[{ totalRow }]] = await DB.query(sql, shopSid)
-
-
-
-  res.json(totalRow)
+  const shopNameSql = "SELECT `name`,`src` FROM `shop` WHERE `sid` = ?"
+  const [[{name,src}]] = await DB.query(shopNameSql,shopSid)
+  const output = {}
+  output.totalRow= totalRow
+  output.name=name
+  output.src=src
+  res.json(output)
 })
 
 
@@ -22,7 +25,7 @@ router.get("/getAllList", async (req, res) => {
   const shopSid = req.query.shopSid
   const pageNow = req.query.getPage
   const perPage = 24
-  const sql = "SELECT m.`name`,se.`sid`, se.`order_sid`, se.`member_sid`, se. `evaluation_score`, se.`evaluation_content`, se.`evaluation_time`   FROM `shop_evaluation` se LEFT JOIN `member` m ON se.`member_sid` = m.`sid`  WHERE  se.`shop_sid` =?  ORDER BY se.`evaluation_time` DESC LIMIT ?,?"
+  const sql = "SELECT m.`name`,m.`image`,se.`sid`, se.`order_sid`, se.`member_sid`, se. `evaluation_score`, se.`evaluation_content`, se.`evaluation_time`   FROM `shop_evaluation` se LEFT JOIN `member` m ON se.`member_sid` = m.`sid`  WHERE  se.`shop_sid` =?  ORDER BY se.`evaluation_time` DESC LIMIT ?,?"
   // const sql = "SELECT m.`name`,se.`sid`, se.`order_sid`, se.`member_sid`, se. `evaluation_score`, se.`evaluation_content`, se.`evaluation_time`   FROM `shop_evaluation` se LEFT JOIN `member` m ON se.`member_sid` = m.`sid`  WHERE  se.`shop_sid` =?  ORDER BY se.`sid`  LIMIT ?,?"
 
   //${(queryPage - 1) * perPage}, ${perPage}
