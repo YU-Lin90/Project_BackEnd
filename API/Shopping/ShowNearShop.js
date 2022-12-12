d3a7a1c033fc2618f52ea6e314e4733f5a823bff
YuChenLin <andy50152@gmail.com>
2022-12-12 12:34:06 +0800

    附近店家修改

--- API/Shopping/ShowNearShop.js
+++ API/Shopping/ShowNearShop.js
@@ -9,7 +9,7 @@ router.get("/", async (req, res) => {
   const maxDistance = req.query.maxDistance
   const sql = "SELECT  `name`,`sid`,`address`,`phone`,`average_evaluation`, `shop_lat`,`shop_lng`,(6378.137 *  2 * asin(SQRT(sin(((( ? - `shop_lat`) / 2) * PI()) / 180) * sin((( ( ? - `shop_lat`)  / 2) * PI()) / 180) + cos(( `shop_lat`  * PI()) / 180) * cos((? * PI()) / 180) * sin((((? - `shop_lng`) / 2) * PI()) / 180) * sin((( (  ?  -  `shop_lng`)  / 2) * PI()) / 180) ))) DE  FROM `shop` WHERE  6378.137 *  2 * asin(SQRT(sin(((( ? - `shop_lat`) / 2) * PI()) / 180) * sin((( ( ? - `shop_lat`)  / 2) * PI()) / 180) + cos(( `shop_lat`  * PI()) / 180) * cos((? * PI()) / 180) * sin((((? - `shop_lng`) / 2) * PI()) / 180) * sin((( (  ?  -  `shop_lng`)  / 2) * PI()) / 180) )) <? ORDER BY DE"
   const [result] = await DB.query(sql,[lat,lat,lng,lng,lng,lat,lat,lng,lng,lng,maxDistance])
-
+  console.log(result);
   res.json(result)
 })
 
